d4b22787ef156f1035f5a3c8218be7c565c3e990
Staninna <stanrochat15@gmail.com>
2021-11-06 17:42:38 +0000

    Done For Today

--- options template.py
+++ options template.py
@@ -1,7 +1,7 @@
 CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"      # Your client_id
 CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"  # Your client_secret
 
-SERVER_NAME = "AFK_SPOTIFY_CLIENT"                  # Name of device that is going to afk
+SERVER_NAME = "AFK_SPOTIFY_CLIENT_"                  # Name of device that is going to afk
 PLAYLIST_NAME = "AFK_PLAYLIST"                      # Name of playlist to grab the songs from
 CYCLE_TIME = .5 * 60                                # Time in seconds between music playback checks, when song skip is enabled, likely smooths to 0 
 UPDATE_PLAYLIST = 12                                # Time to wait the CYCLE_TIME before updating playlist calculation is (CYCLE_TIME * UPDATE_PLAYLIST)
@@ -10,4 +10,4 @@ RANDOM_ORDER_SONGS = True                           # Do you want to randomize t
 
 # There also some things i didn't mention in this file about timings becomeing really long depending on your length of your afk playlist and enabling track skipping
 
-# rename tis file to options.py
+# rename this file to options.py
