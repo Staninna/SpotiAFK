5707cebc4ea948a8bfd5bec294474f8c458b7c9b
Staninna <stanrochat15@gmail.com>
2021-11-07 18:15:06 +0000

    DONE FOR TODAY AND SHIT LMAO IM AT MY GURLSFRIENDS HOUSE SO BYE LAPTOP

--- spotAFK.py
+++ spotAFK.py
@@ -72,16 +72,82 @@ Spotify = SpotifyAPI(client_id,
                      tokens_path)
 Spotify.auth()
 
+
 # SETTINGS
 
-SERVER_NAME = options.SERVER_NAME
+SERVER_NAMES = options.SERVER_NAMES
 PLAYLIST_NAME = options.PLAYLIST_NAME
-CYCLE_TIME = options.CYCLE_TIME
-UPDATE_PLAYLIST = options.UPDATE_PLAYLIST
-RANDOM_ORDER_SONGS = options.RANDOM_ORDER_SONGS
+TIME_BETWEEN_CHEAKS = options.TIME_BETWEEN_CHEAKS
+CHEAKS_BEFORE_PLAYING = options.CHEAKS_BEFORE_PLAYING
+RANDOM_ORDER_TRACKS = options.RANDOM_ORDER_TRACKS
+SKIP_SONGS = options.SKIP_SONGS
 SKIP_DELAY = options.SKIP_DELAY
+
 
 # CODE
 
-# TODO GET DEVICE_ID OF SERVER_NAME
+# FUNCTIONS
 
+def can_i_play(succes_checks):
+    try:
+        if Spotify.client.current_user_playing_track()["is_playing"]:  
+                for device in Spotify.client.devices()["devices"]:
+                    if device["is_active"]:
+                        if device["name"] in SERVER_NAMES:
+                            succes_checks += 1
+                        else:
+                            succes_checks = 0
+        else:
+            succes_checks += 1
+    except TypeError:
+        succes_checks += 1
+    return succes_checks
+
+def update_playlist():
+    playlists = Spotify.client.current_user_playlists()
+    for playlist in playlists["items"]:
+        if playlist["name"] == PLAYLIST_NAME:
+            tracks = Spotify.client.playlist(playlist["id"])["tracks"]
+            tracks_to_play = list()
+            while tracks:
+                for track in tracks["items"]:
+                    duration_sec = track["track"]["duration_ms"] / 1000
+                    uri = track["track"]["uri"]
+                    tracks_to_play.append([uri, duration_sec])
+                if tracks["next"]:
+                    tracks = Spotify.client.next(tracks)
+                else:
+                    tracks = None
+            break
+    if RANDOM_ORDER_TRACKS:
+        random.shuffle(tracks_to_play)
+    return tracks_to_play
+
+# Get Server ids
+devices = Spotify.client.devices()
+for device in devices["devices"]:
+    if device["name"] == SERVER_NAMES:
+        server_id = device["id"]
+
+succes_checks = 0
+# MAIN LOOP
+while True:
+    try:
+        time.sleep(TIME_BETWEEN_CHEAKS)
+        succes_checks = can_i_play(succes_checks)
+        while succes_checks >= CHEAKS_BEFORE_PLAYING:
+            Spotify.client.transfer_playback(server_id, False)
+            tracks = update_playlist()
+            for track, duration in tracks:
+                if can_i_play(0) == 0:
+                    break
+                Spotify.client.add_to_queue(track)
+                Spotify.client.next_track()
+                if SKIP_SONGS:
+                    time.sleep(SKIP_DELAY)
+                else:
+                    time.sleep(duration)
+            time.sleep(TIME_BETWEEN_CHEAKS)
+            succes_checks = can_i_play(succes_checks)
+    except Exception as e:
+        print(e)
