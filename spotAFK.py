34d952f9c37426a23e06d085575063e8aae73205
Staninna <stanrochat15@gmail.com>
2021-11-15 14:36:06 +0100

    added logging printing

--- spotAFK.py
+++ spotAFK.py
@@ -80,8 +80,6 @@ Spotify = API(client_id,
 
 # SETTINGS
 
-# TODO Make options gui
-
 SERVER_NAMES = options.SERVER_NAMES
 PLAYLIST_NAME = options.PLAYLIST_NAME
 TIME_BETWEEN_CHEAKS = options.TIME_BETWEEN_CHEAKS
@@ -169,6 +167,11 @@ def get_server_ids():
             logging.info("Retrying updateing playlist")
     return server_ids
 
+def log(level, 
+        message):
+    print(message)
+    logging.log(level, message)
+
 # Making Log File
 if not os.path.isdir("logs"):
     os.mkdir("logs")
@@ -178,7 +181,7 @@ logging.basicConfig(filename=f"logs/{date.day}-{date.month}-{date.year}_{date.ho
                     format="%(asctime)s %(levelname)s: %(message)s",
                     datefmt='%d/%m/%Y %H:%M:%S:'
                     )
-logging.info("Started the program")
+log(logging.INFO, "Started the program")
 
 # Auth
 Spotify.auth(RETRY_TIME)
@@ -196,45 +199,46 @@ while True:
             played = False
         while succes_checks >= CHEAKS_BEFORE_PLAYING:
             if not played:
-                logging.info("Started playing tracks")
+                log(logging.INFO, "Started playing tracks")
                 played = True
             while True:
                 try:
                     Spotify.client.transfer_playback(server_ids[0], False)
                     break
                 except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-                    logging.info("No internet connection found while transfering playback to server")
+                    log(logging.INFO, "No internet connection found while transfering playback to server")
                     time.sleep(RETRY_TIME)
-                    logging.info("Retrying transfering playback to server")
+                    log(logging.INFO, "Retrying transfering playback to server")
             tracks = update_playlist(RETRY_TIME)
             for track, duration, name in tracks:
                 if can_i_play(0, RETRY_TIME) == 0:                    
-                    logging.info("Stopped playing tracks")
+                    log(logging.INFO, "Stopped playing tracks")
                     break
                 while True:
                     try:
                         Spotify.client.add_to_queue(track)
                         break
                     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-                        logging.info("No internet connection found while adding track to queue")
+                        log(logging.INFO, "No internet connection found while adding track to queue")
                         time.sleep(RETRY_TIME)
-                        logging.info("Retrying adding track to queue")
+                        log(logging.INFO, "Retrying adding track to queue")
                 while True:
                     try:
                         Spotify.client.next_track()
                         break
                     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-                        logging.info("No internet connection found while skipping track")
+                        log(logging.INFO, "No internet connection found while skipping track")
                         time.sleep(RETRY_TIME)
-                        logging.info("Retrying skipping track")
+                        log(logging.INFO, "Retrying skipping track")
                 if SKIP_SONGS:
                     time.sleep(SKIP_DELAY)
                 else:
                     time.sleep(duration)
-                logging.info(f"Played {name}")
+                log(logging.INFO, f"Played {name}")
             time.sleep(TIME_BETWEEN_CHEAKS)
             succes_checks = can_i_play(succes_checks, RETRY_TIME)
     except Exception as e:
-        logging.error(e)
+        time.sleep(RETRY_TIME)
+        log(logging.ERROR, e)
         Spotify.auth(RETRY_TIME)
         server_ids = get_server_ids()
