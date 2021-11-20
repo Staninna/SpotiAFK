b08dde82045e566acd1295273ebc3c7fcc212e5b
Staninna <26458805+Staninna@users.noreply.github.com>
2021-11-20 16:19:29 +0000

    Update options template.py

--- options template.py
+++ options template.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 SKIP_DELAY = 35
 RETRY_TIME = 10
 SKIP_SONGS = True
@@ -10,48 +9,3 @@ TIME_BETWEEN_CHEAKS = .5 * 60
 CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 SERVER_NAMES = ["SERVER-1", "SERVER-2", "SERVER-3", "SERVER-4"]
-=======
-# API keys
-
-# Your client secret
-CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-# Your client id
-CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-
-# Cheaks
-
-# Time in between cheaks
-TIME_BETWEEN_CHEAKS = .5 * 60
-# Cheaks before playing
-CHEAKS_BEFORE_PLAYING = 5
-
-
-# Playlist
-
-# Name of the playlist
-PLAYLIST_NAME = "PLAYLIST"
-# If you want to randomize your playlist
-RANDOM_ORDER_TRACKS = True
-
-
-# Names
-
-# Your server names
-SERVER_NAMES = ["SERVER-1", "SERVER-2", "SERVER-3", "SERVER-4"]
-# Your spotify username
-USERNAME = "USERNAME"
-
-
-# Skipping
-
-# If you want to skip songs after a certain amount of seconds
-SKIP_SONGS = True
-# Time to wait before skipping a song
-SKIP_DELAY = 35
-
-
-# Errors
-
-# Time to wait before retrying after a error occurs
-RETRY_TIME = 10
->>>>>>> 1c8fee42561ef49f5278654bea39586c5b4818d1
