1c8fee42561ef49f5278654bea39586c5b4818d1
Staninna <stanrochat15@gmail.com>
2021-11-20 16:45:28 +0100

    Polished my code

--- options template.py
+++ options template.py
@@ -1,11 +1,43 @@
-CLIENT_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-CLIENT_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# API keys
 
-SERVER_NAMES = ["AFK_SPOTIFY_CLIENT", "DESKTOP-SERVER"]
-PLAYLIST_NAME = "AFK_PLAYLIST"
-TIME_BETWEEN_CHEAKS = .5 * 60 # 30 Seconds
-CHEAKS_BEFORE_PLAYING = 2
+# Your client secret
+CLIENT_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# Your client id
+CLIENT_ID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+# Cheaks
+
+# Time in between cheaks
+TIME_BETWEEN_CHEAKS = .5 * 60
+# Cheaks before playing
+CHEAKS_BEFORE_PLAYING = 5
+
+
+# Playlist
+
+# Name of the playlist
+PLAYLIST_NAME = "PLAYLIST"
+# If you want to randomize your playlist
 RANDOM_ORDER_TRACKS = True
+
+
+# Names
+
+# Your server names
+SERVER_NAMES = ["SERVER-1", "SERVER-2", "SERVER-3", "SERVER-4"]
+# Your spotify username
+USERNAME = "USERNAME"
+
+
+# Skipping
+
+# If you want to skip songs after a certain amount of seconds
 SKIP_SONGS = True
+# Time to wait before skipping a song
 SKIP_DELAY = 35
+
+
+# Errors
+
+# Time to wait before retrying after a error occurs
 RETRY_TIME = 10
