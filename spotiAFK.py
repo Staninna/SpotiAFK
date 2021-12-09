c11eb228cd000047b507f5e8f8df469cf0f1524d
Staninna <26458805+Staninna@users.noreply.github.com>
2021-12-09 19:36:14 +0100

    🔥 REMOVED SPACES

--- spotiAFK.py
+++ spotiAFK.py
@@ -262,7 +262,7 @@ while True:
             
             # Looping over songs
             for track, duration, name in tracks:
-                if can_i_play(0, RETRY_TIME, lost_time)[0] == 0:                    
+                if can_i_play(0, RETRY_TIME, lost_time)[0] == 0:
                     log(logging.INFO, "Stopped playing tracks")
                     with open(TIMELOG_FILENAME, "r") as f:
                         total_time = float(f.readline()) + (start_playing_time - lost_time - time.time())
